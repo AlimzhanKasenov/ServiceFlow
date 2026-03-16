e24f6a555c4122c567c4e4330f09ea8f0f688131
Alimzhan Kassenov <alim_kasenov@bk.ru>
2026-03-16 09:51:03 +0500

    Уведомления

--- app/Events/RequestStageChanged.php
+++ app/Events/RequestStageChanged.php
@@ -7,17 +7,48 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 /**
- * Событие смены стадии заявки
+ * Class RequestStageChanged
+ *
+ * Domain Event: смена стадии заявки.
+ *
+ * Генерируется при любом перемещении заявки между стадиями.
+ *
+ * Используется для:
+ *
+ * - AutomationEngine (роботы)
+ * - Webhooks
+ * - Notifications
+ * - SLA watchers
+ * - Analytics
+ *
+ * Пример:
+ *
+ * Request #15
+ * from_stage: New
+ * to_stage: In Progress
  */
 class RequestStageChanged
 {
     use Dispatchable, SerializesModels;
 
+    /**
+     * Заявка
+     */
     public ServiceRequest $request;
 
+    /**
+     * Стадия из которой перешли
+     */
     public int $fromStageId;
+
+    /**
+     * Стадия в которую перешли
+     */
     public int $toStageId;
 
+    /**
+     * Конструктор события
+     */
     public function __construct(
         ServiceRequest $request,
         int $fromStageId,
@@ -26,5 +57,21 @@ class RequestStageChanged
         $this->request = $request;
         $this->fromStageId = $fromStageId;
         $this->toStageId = $toStageId;
+    }
+
+    /**
+     * Получить pipeline заявки
+     */
+    public function pipelineId(): int
+    {
+        return $this->request->pipeline_id;
+    }
+
+    /**
+     * Получить ID заявки
+     */
+    public function requestId(): int
+    {
+        return $this->request->id;
     }
 }
